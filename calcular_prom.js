ad3e245105694fd0262fc549815967e9fc1ddee4
federico castillo <castillo.fmp@gmail.com>
2023-05-22 10:52:52 -0300

    primera pre entrega js

--- calcular_prom.js
+++ calcular_prom.js
@@ -1,34 +1,34 @@
     function calcularPromedio(notas) {
-        var suma = 0;
-        for (var i = 0; i < notas.length; i++) {
+        let suma = 0;
+        for (let i = 0; i < notas.length; i++) {
           suma += notas[i];
         }
         return suma / notas.length;
       }
       
       // Variable para almacenar los alumnos
-      var alumnos = [];
+      let alumnos = [];
       
       // Variable para controlar si se deben ingresar más alumnos
-      var ingresarMasAlumnos = true;
+      let ingresarMasAlumnos = true;
       
       // Ciclo para ingresar los alumnos
       while (ingresarMasAlumnos) {
         // Pedir al usuario el nombre del alumno
-        var nombreAlumno = prompt("Ingrese el nombre del alumno:");
+        let nombreAlumno = prompt("Ingrese el nombre del alumno:");
         
       
         // Array para almacenar las notas del alumno
-        var notas = [];
+        let notas = [];
       
         // Ciclo para ingresar las notas del alumno
-        for (var i = 0; i < 3; i++) {
-          var nota = parseFloat(prompt("Ingrese la nota " + (i + 1) + " del alumno " + nombreAlumno + ":"));
+        for (let i = 0; i < 3; i++) {
+          let nota = parseFloat(prompt("Ingrese la nota " + (i + 1) + " del alumno " + nombreAlumno + ":"));
           notas.push(nota);
         }
       
         // Calcular el promedio del alumno
-        var promedioAlumno = calcularPromedio(notas);
+        let promedioAlumno = calcularPromedio(notas);
       
         // Agregar el alumno y su promedio al array de alumnos
         alumnos.push({
@@ -38,7 +38,7 @@
         });
       
         // Preguntar si se desean ingresar más alumnos
-        var opcion = prompt("¿Desea ingresar más alumnos? (s/n)");
+        let opcion = prompt("¿Desea ingresar más alumnos? (s/n)");
       
         // Verificar la respuesta para continuar o salir del ciclo
         if (opcion.toLowerCase() !== "s") {
@@ -47,8 +47,8 @@
       }
       
       // Mostrar los datos de los alumnos
-      for (var i = 0; i < alumnos.length; i++) {
-        var alumno = alumnos[i];
+      for (let i = 0; i < alumnos.length; i++) {
+        let alumno = alumnos[i];
         console.log("Alumno: " + alumno.nombre);
         console.log("Notas: " + alumno.notas.join(", "));
         console.log("Promedio: " + alumno.promedio.toFixed(2));
